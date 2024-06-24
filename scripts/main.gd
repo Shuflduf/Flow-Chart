a47a908acdc3a81f52dd297358baeef5b4095ca5
Shuflduf <shuflduf@gmail.com>
2024-06-23 20:18:42 -0600

    stretching windows works now

--- scripts/main.gd
+++ scripts/main.gd
@@ -34,6 +34,7 @@ func move_grid():
 		
 func _on_gui_input(event):
 	if event.is_action_pressed("mouse_left"):
+		print("backround")
 		for child in get_children():
 			if child is FlowChartNode:
 				child._on_text_edit_focus_exited()
