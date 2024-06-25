6a616a2bf4f58628801ff9457a00d1d6515d756d
Shuflduf <shuflduf@gmail.com>
2024-06-24 21:10:47 -0600

    im TRYING to make it WORK

--- scripts/node.gd
+++ scripts/node.gd
@@ -5,6 +5,7 @@ signal picked_up
 
 
 @onready var text = %TextEdit
+@onready var text_box = %TextBox
 @onready var handles = $Handles
 
 var local_mouse_offset: Vector2
@@ -15,26 +16,34 @@ func _ready():
 		if handle is Handle:
 			handle.handle_grabbed.connect(func(): print("dkjfgdfjkg"))
 			
-func _on_gui_input(event):
+func _on_text_edit_gui_input(event):
+	#print(text_box.position - event.position)
+	if event is InputEventMouse:
+		_on_gui_input(event)
+	
+func _on_gui_input(event: InputEventMouse):
 	if event.is_action_pressed("mouse_left"):
-		local_mouse_offset = -event.position
-		
+		local_mouse_offset = global_position - event.global_position
+		#print(local_mouse_offset.length())
 		var moving := false
+		var temp : float
 		while !moving and holding_down():
 			await get_tree().process_frame
-			moving = (local_mouse_offset + get_local_mouse_position()).length()\
-				 > Global.settings.mouse_margin
+			temp = (local_mouse_offset - \
+				(text_box.global_position - get_local_mouse_position())).length()
+			moving = temp > Global.settings.mouse_margin
 		
+		print(local_mouse_offset)
 		if moving:
 			pickup()
 		else:
-			text.editable = true
+			enable_text_field()
 			
 	if event.is_action_released("mouse_left"):
 		drop()
 		
 func pickup():
-	_on_text_edit_focus_exited()
+	disable_text_field()
 	Global.whos_on_top.push_front(self)
 	Global.verify_on_top()
 		
@@ -58,12 +67,16 @@ func clamp_zindex():
 func holding_down():
 	return Input.is_action_pressed("mouse_left")
 
-func _on_text_edit_focus_exited():
+func disable_text_field():
 	text.editable = false
+	
+
+func enable_text_field():
+	text.editable = true
 
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
-		_on_text_edit_focus_exited()
+		disable_text_field()
 
 func update_handles_position():
 	var handles_children = handles.get_children()
@@ -72,4 +85,7 @@ func update_handles_position():
 	handles_children[2].position = Vector2(size.x, size.y / 2) - handles_children[2].size / 2
 	handles_children[3].position = Vector2(size.x / 2, 0) - handles_children[3].size / 2
 		
+
+
+
 
