172a3d8e68caaf0992418cf784c94e27c950aced
Shuflduf <shuflduf@gmail.com>
2024-06-30 15:11:54 -0600

    zooming works perfectly

--- scripts/chart.gd
+++ scripts/chart.gd
@@ -55,15 +55,22 @@ func _on_gui_input(event: InputEvent) -> void:
 		moving_chart = false
 		return
 		
-	if event.is_pressed():
-		nodes.pivot_offset = get_global_mouse_position()
-		print(pivot_offset)
-		match event.button_index:
-			MOUSE_BUTTON_WHEEL_UP:
-				cam_zoom += Global.settings.zoom_sens
-				nodes.scale = Vector2(cam_zoom, cam_zoom)
-				#nodes.position += get_global_mouse_position() * Vector2(cam_zoom, cam_zoom)
-			MOUSE_BUTTON_WHEEL_DOWN:
-				cam_zoom -= Global.settings.zoom_sens
-				nodes.scale = Vector2(cam_zoom, cam_zoom)
-				#nodes.position += get_global_mouse_position() * Vector2(cam_zoom, cam_zoom)
+	if event is InputEventMouse:
+		if event.is_pressed() and not event.is_echo():
+			var mouse_position: Vector2 = event.global_position
+			match event.button_index:
+				MOUSE_BUTTON_WHEEL_UP:
+					_zoom_at_point(Global.settings.zoom_sens, mouse_position)
+				MOUSE_BUTTON_WHEEL_DOWN:
+					_zoom_at_point(1 / Global.settings.zoom_sens, mouse_position)
+
+
+func _zoom_at_point(zoom_change: float, mouse_position: Vector2) -> void:
+	print(nodes.scale)
+	if (!nodes.scale > Vector2(10, 10) or zoom_change < 1)\
+			and (!nodes.scale < Vector2(0.2, 0.2) or zoom_change > 1):
+		nodes.scale = nodes.scale * zoom_change
+		var delta := (mouse_position - nodes.global_position) * (zoom_change - 1)
+		nodes.global_position = nodes.global_position - delta
+	#nodes.scale = clamp(nodes.scale, Vector2(0.1, 0.1), Vector2(10, 10))
+
--- scripts/settings.gd
+++ scripts/settings.gd
@@ -7,4 +7,4 @@ extends Resource
 @export var min_size := 64
 @export var max_size := 512
 @export var inverse_scroll := false
-@export_range(0.0, 1.0, 0.1) var zoom_sens := 0.1
+@export_range(0.0, 1.0, 0.1) var zoom_sens := 1.1
