84e3e5170b6e23917035c6a8772fad2541fd5bcc
Shuflduf <shuflduf@gmail.com>
2024-06-30 14:19:50 -0600

    moving around the chart works perfectly

--- scripts/chart.gd
+++ scripts/chart.gd
@@ -4,15 +4,20 @@ var grid_mover := GridMover.new()
 var moving_chart := false
 var mouse_offset: Vector2
 
+@onready var nodes: Control = $Nodes
+
 func _process(_delta: float) -> void:
+	
+	
 	if Global.active_node != null:
 		Global.active_node._moved()
 		var actual_pos := get_local_mouse_position() + Global.active_node.local_mouse_offset
 
-		Global.active_node.position = grid_mover.move_grid(actual_pos)
+		Global.active_node.global_position = grid_mover.move_grid(actual_pos)
 		return
+		
 	if moving_chart:
-		global_position = get_global_mouse_position() + mouse_offset
+		nodes.position = get_global_mouse_position() + mouse_offset
 
 func _ready() -> void:
 	for child in get_children():
@@ -31,17 +36,18 @@ func move_all_nodes_down() -> void:
 	
 
 
-func _on_sub_viewport_container_gui_input(event: InputEvent) -> void:
 
+func _on_gui_input(event: InputEvent) -> void:
 	if event.is_action_pressed("mouse_left"):
-		mouse_offset = global_position - event.global_position
+		mouse_offset = nodes.global_position - event.global_position
 		var margin_handler := MouseMarginer.new()
 		margin_handler.start_pos = mouse_offset
+		
 		while !moving_chart and Input.is_action_pressed("mouse_left"):
 			await get_tree().process_frame
 			if margin_handler.passed_threshold(get_local_mouse_position()):
 				moving_chart = true
-					
+						
 	if event.is_action_released("mouse_left"):
 		moving_chart = false
 		print("jhfg")
