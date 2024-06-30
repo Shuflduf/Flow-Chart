2b8f8613bff87bf6018b882347272168567dac78
Shuflduf <shuflduf@gmail.com>
2024-06-30 12:33:26 -0600

    clamped the size

--- scripts/node.gd
+++ scripts/node.gd
@@ -146,6 +146,7 @@ func _on_margin_container_gui_input(event: InputEvent) -> void:
 		
 			
 		size.x = clamp(size.x, Global.settings.min_size, Global.settings.max_size)	
+		size.y = clamp(size.y, Global.settings.min_size, Global.settings.max_size)	
 		update_handles_position()
 			 	
 		
